a7cfa6577ae0f3a1d731bd72cfbf8c44b92485d6
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2022-04-21 17:29:52 +0100

    Update run_bactmap.sh

--- run_bactmap.sh
+++ run_bactmap.sh
@@ -10,25 +10,26 @@ export NXF_VER=21.10.6
 DATA_DIR=$1
 REF=$2
 
+RAND=$(date +%s%N | cut -b10-19)
+OUT_DIR=${DATA_DIR}/bactmap-1.0.0_${RAND}
 NEXTFLOW_PIPELINE_DIR='/home/vagrant/nf-pipelines/nf-core-bactmap-1.0.0'
-#OUT_DIR - randomise with the date and time
 
-echo $NEXTFLOW_PIPELINE_DIR
-echo $DATA_DIR
-echo $REF
+echo "Pipeline is: "$NEXTFLOW_PIPELINE_DIR
+echo "Input data is: "$DATA_DIR
+echo "Output will be written to: "$OUT_DIR
 
 nextflow run ${NEXTFLOW_PIPELINE_DIR}/workflow/main.nf \
 --input ${DATA_DIR}/samplesheet.csv \
---outdir ${DATA_DIR}/bactmap-1.0.0 \
+--outdir ${OUT_DIR} \
 --reference ${REF} \
 --iqtree \
--w ${DATA_DIR}/bactmap-1.0.0/work \
--profile singularity
--with-tower -qs 32 -resume
-#-c ${NEXTFLOW_PIPELINE_DIR}/user.config \
+-w ${OUT_DIR}/work \
+-profile singularity \
+-with-tower -resume \
+-c /home/software/nf_pipeline_scripts/bakersrv1.config
 
 # Clean up on sucess/exit 0
 status=$?
 if [[ $status -eq 0 ]]; then
-  rm -r ${DATA_DIR}/bactmap-1.0.0/work
+  rm -r ${OUT_DIR}/work
 fi
