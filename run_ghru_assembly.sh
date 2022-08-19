653fe758c6d400f2dd8faa0ef67dfe2e5168ec60
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2022-08-19 11:06:25 +0100

    Update run_ghru_assembly.sh

--- run_ghru_assembly.sh
+++ run_ghru_assembly.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# Author: Jacqui Keane <drjkeane at gmail.com>
+# URL:    https://www.cambridgebioinformatics.com
+#
+# Usage: run_ghru_assembly.sh [-h] -i input_directory -o output_directory
+#
+
+set -eu
 
 export NXF_ANSI_LOG=false
 export NXF_OPTS="-Xms8G -Xmx8G -Dnxf.pool.maxThreads=2000"
@@ -9,7 +17,7 @@ function help
    # Display Help
    script=$(basename $0)
    echo 
-   echo "usage: "$script" [-h] -i input_directory"
+   echo "usage: "$script" [-h] -i input_directory -o output_directory"
    echo
    echo "Runs the ghru assembly nextflow pipeline, see https://gitlab.com/cgps/ghru/pipelines/dsl2/pipelines/assembly"
    echo
@@ -18,16 +26,15 @@ function help
    echo
    echo "required arguments:"
    echo "  -i input_directory   directory containing the FASTQ files to be assembled"
+   echo "  -o output_directory  directory to write the pipeline results to"
    echo
    echo "To run this pipeline with alternative parameters, copy this script and make changes to nextflow run as required"
    echo
 }
 
 # Check number of input parameters 
-
 NAG=$#
-
-if [ $NAG -ne 1 ] && [ $NAG -ne 2 ] && [ $NAG -ne 3 ]
+if [ $NAG -ne 1 ] && [ $NAG -ne 4 ] && [ $NAG -ne 5 ]
 then
   help
   echo "!!! Please provide the correct number of input arguments"
@@ -43,30 +50,46 @@ while getopts "hi:" option; do
          exit;;
       i) # Input directory
          INPUT_DIR=$OPTARG;;
+      o) # Input directory
+         OUTPUT_DIR=$OPTARG;;
      \?) # Invalid option
          help
-         echo "!!!Error: Invalid arguments"
+         echo "!!! Error: Invalid arguments"
          exit;;
    esac
 done
 
+# Check the input directory exists
 if [ ! -d $INPUT_DIR ]
 then
   help
-  echo "!!! The directory $INPUT_DIR does not exist"
+  echo "!!! The input directory $INPUT_DIR does not exist"
   echo
   exit;
 fi
 
+# Check the output directory exists
+if [ ! -d $OUTPUT_DIR ]
+then
+  help
+  echo "!!! The output directory $OUTPUT_DIR does not exist"
+  echo
+  exit;
+fi
+
+# Create a unique directory for the output
 RAND=$(date +%s%N | cut -b10-19)
-OUT_DIR=${INPUT_DIR}/ghru-assembly-2.1.2_${RAND}
+OUT_DIR=${OUTPUT_DIR}/ghru-assembly-2.1.2_${RAND}
 WORK_DIR=${OUT_DIR}/work
+
+# Set the pipeline directory
 NEXTFLOW_PIPELINE_DIR='/home/software/nf-pipelines/assembly-2.1.2'
 
 echo "Pipeline is: "$NEXTFLOW_PIPELINE_DIR
 echo "Input data is: "$INPUT_DIR
 echo "Output will be written to: "$OUT_DIR
 
+# Run the pipeline
 nextflow run \
 ${NEXTFLOW_PIPELINE_DIR}/main.nf \
 --adapter_file ${NEXTFLOW_PIPELINE_DIR}/adapters.fas \
@@ -81,8 +104,10 @@ ${NEXTFLOW_PIPELINE_DIR}/main.nf \
 -with-tower -resume \
 -c /home/software/nf_pipeline_scripts/conf/bakersrv1.config,/home/software/nf_pipeline_scripts/conf/pipelines/ghru_assembly.config
 
-# Clean up on sucess/exit 0
+# Clean up on success (exit 0)
 status=$?
 if [[ $status -eq 0 ]]; then
   rm -r ${WORK_DIR}
 fi
+
+set +eu
