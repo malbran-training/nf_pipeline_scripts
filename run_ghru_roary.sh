13f07c78f641ccc8a4db24d1294d47f54e6da57a
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2023-08-24 10:19:40 +0100

    Update run_ghru_roary.sh

--- run_ghru_roary.sh
+++ run_ghru_roary.sh
@@ -10,7 +10,7 @@ set -eu
 
 export NXF_ANSI_LOG=false
 export NXF_OPTS="-Xms8G -Xmx8G -Dnxf.pool.maxThreads=2000"
-export NXF_VER=21.10.6
+export NXF_VER=23.04.1
 
 function help
 {
@@ -83,7 +83,7 @@ OUT_DIR=${OUTPUT_DIR}/ghru-roary-1.1.4_${RAND}
 WORK_DIR=${OUT_DIR}/work
 
 # Set the location of the pipeline
-NEXTFLOW_PIPELINE_DIR='/home/software/nf-pipelines/roary-1.1.4'
+NEXTFLOW_PIPELINE_DIR='/home/manager/nf-pipelines/roary-1.1.4'
 
 echo "Pipeline is: "$NEXTFLOW_PIPELINE_DIR
 echo "Input data is: "$INPUT_DIR
@@ -99,7 +99,7 @@ ${NEXTFLOW_PIPELINE_DIR}/roary.nf \
 --tree \
 -w ${WORK_DIR} \
 -with-tower -resume \
--c /home/software/nf_pipeline_scripts/conf/bioinfsrv1.config,/home/software/nf_pipeline_scripts/conf/pipelines/ghru_roary.config
+-c /home/manager/nf_pipeline_scripts/conf/bioinfsrv1.config,/home/manager/nf_pipeline_scripts/conf/pipelines/ghru_roary.config
 
 # Clean up on success (exit 0)
 status=$?
